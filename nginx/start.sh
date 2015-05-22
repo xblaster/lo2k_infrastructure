99dbb56f6faa45d7a9a046f1bd5b44dfd70fd65b
Jérôme <nospam@lo2k.net>
2015-05-22 13:39:31 +0000

    configure proxy

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,2 +1,7 @@
-docker rm nginx_balancer
-docker run -p 80:80 --name nginx_balancer -v ~/lo2k_infrastructure/nginx/sites-enabled:/etc/nginx/sites-enabled -v  ~/lo2k_infrastructure/nginx/logs:/var/log/nginx -v  ~/lo2k_infrastructure/nginx/html:/var/www --link weblog_wordpress_1:weblog xblaster/nginx nginx
+docker rm -f nginx_balancer
+docker run -d -p 80:80 --name nginx_balancer \
+       -v ~/lo2k_infrastructure/nginx/sites-enabled:/etc/nginx/sites-enabled \
+       -v  ~/lo2k_infrastructure/nginx/logs:/var/log/nginx \
+       -v  ~/lo2k_infrastructure/nginx/html:/var/www \
+       --link weblog_wordpress_1:weblog \
+       xblaster/nginx  
